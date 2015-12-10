e6d7d339f120aeb9bfdfa8631d1b55471562e5f0
nitsirk <kristinblackwell@gmail.com>
2015-12-09 20:20:33 -0500

    users add words to dictionary

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,8 +3,17 @@ get '/words' do
   erb :"/words/index"
 end
 
+get '/words/new' do
+  erb :"/words/new"
+end
+
 get '/words/:id' do
   id = params[:id]
   @word = Word.find(id)
   erb :"/words/show"
+end
+
+post '/words' do
+  word = Word.create(text: params[:text])
+  redirect "/words/#{word.id}"
 end
